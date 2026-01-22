420a2dd7648178f01bc3e54785d4e51d6a71b978
rahulpatel902 <mp902785@gmail.com>
2026-01-22 23:32:40 +0530

    Add landing page with mobile button centering fix

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -97,7 +97,7 @@ const Landing = () => {
         </div>
 
         {/* CTA Buttons */}
-        <div className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col sm:flex-row items-center gap-4">
           <Link to="/login">
             <Button 
               size="lg" 
